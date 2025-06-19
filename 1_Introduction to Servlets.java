a3089591eceef3bfe9360b7b0f2b46a2adaee730
unstoppablesahilsaifi <97735834+unstoppablesahilsaifi@users.noreply.github.com>
2025-06-19 16:52:54 +0530

    Create 1_Introduction to Servlets.java
    added

---
+++ 1_Introduction to Servlets.java
@@ -0,0 +1,2 @@
+What is a Servlet?
+A Servlet is a Java class that handles HTTP requests and generates responses, usually in a web application. It runs inside a Servlet container (like Apache Tomcat).
